687c8697bae937abf24c77573a5b7c146bd8cea2
Ben Hollis <ben@benhollis.net>
2019-10-21 23:09:51 -0700

    Initial server setup

---
+++ README.md
@@ -0,0 +1,3 @@
+1. Go to hub.docker.com, sign up for an account, and download Docker Desktop.
+2. Open Docker preferences, go to the Kubernetes tab, and enable Kubernetes.
+3. Run 'yarn start'
---
+++ api/index.ts
@@ -0,0 +1,8 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hello DIM!!! ' + req.httpVersion));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
