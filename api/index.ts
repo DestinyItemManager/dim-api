4d325e20ac6626da15b155e5a6ddcc44c006c652
Ben Hollis <ben@benhollis.net>
2019-10-26 23:57:40 -0700

    Auth token implementation

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,85 @@
 import express from 'express';
+import apps from './config/apps.json';
+import { AuthTokenRequest, AuthTokenResponse } from './shapes/auth';
+import superagent from 'superagent';
+import asyncHandler from 'express-async-handler';
+import util from 'util';
+
+import { sign, Secret, SignOptions } from 'jsonwebtoken';
+
+const signJwt = util.promisify<
+  string | Buffer | object,
+  Secret,
+  SignOptions,
+  string
+>(sign);
+
+interface ApiApp {
+  bungieApiKey: string;
+  dimApiKey: string;
+}
 
 const app = express();
 const port = 3000;
 
+app.use(express.json()); // for parsing application/json
+
 app.get('/', (req, res) => res.send('Hello DIM!!! ' + req.httpVersion));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.post(
+  '/auth/token',
+  asyncHandler(async (req, res) => {
+    const {
+      app,
+      bungieAccessToken,
+      membershipId
+    } = req.body as AuthTokenRequest;
+
+    const apiApp = apps[app] as ApiApp;
+    if (!apiApp) {
+      res.status(403).send(`App ${app} not registered`);
+    }
+
+    // TODO: validate the rest
+
+    // TODO: error handling (or error handling route?)
+
+    // make request to bungie
+    try {
+      console.log('Sending');
+      const bungieResponse = await superagent
+        .get(
+          'https://www.bungie.net/Platform/User/GetMembershipsForCurrentUser/'
+        )
+        .set('X-API-Key', apiApp.bungieApiKey)
+        .set('Authorization', `Bearer ${bungieAccessToken}`);
+      console.log(bungieResponse.body);
+
+      // TODO: idx
+      const serverMembershipId =
+        bungieResponse.body.Response.bungieNetUser.membershipId;
+      if (serverMembershipId === membershipId) {
+        // generate and return a badass token
+        const token = await signJwt({ foo: 'bar' }, 'shhhhh', {
+          expiresIn: 30 * 24 * 60 * 60
+        });
+
+        const response: AuthTokenResponse = {
+          accessToken: token,
+          expiresInSeconds: 30 * 24 * 60 * 60
+        };
+
+        res.send(response);
+      } else {
+        res.status(403).send(`Hey you're not ${membershipId}`);
+      }
+    } catch (e) {
+      console.log(e);
+      throw new Error(
+        `${e.response.body.ErrorStatus}: ${e.response.body.Message}`
+      );
+    }
+  })
+);
+
+app.listen(port, () => console.log(`DIM API listening on port ${port}!`));
---
+++ api/shapes/auth.ts
@@ -0,0 +1,14 @@
+export interface AuthTokenRequest {
+  app: string;
+  /** The access token from authenticating with the Bungie.net API */
+  bungieAccessToken: string;
+  /** The user's Bungie.net membership ID */
+  membershipId: string;
+}
+
+export interface AuthTokenResponse {
+  /** Your DIM API access token, to be used in further requests. */
+  accessToken: string;
+  /** How many seconds from now the token will expire. */
+  expiresInSeconds: number;
+}
