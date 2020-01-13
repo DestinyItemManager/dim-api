ebed4e3cc1b980a3e4c1b6de797bb05ec93513c0
Ben Hollis <ben@benhollis.net>
2020-01-13 00:02:40 -0800

    Fix error handling

--- api/index.ts
+++ api/index.ts
@@ -8,17 +8,9 @@ import { metrics } from './metrics';
 const app = express();
 const port = 3000;
 
-app.use(metrics.helpers.getExpressMiddleware('http', { timeByUrl: true }));
-app.use(morgan('combined'));
+app.use(metrics.helpers.getExpressMiddleware('http', { timeByUrl: true })); // metrics
+app.use(morgan('combined')); // logging
 app.use(express.json()); // for parsing application/json
-app.use(function(err, _req, res, _next) {
-  if (err.name === 'UnauthorizedError') {
-    res.status(401).send({
-      status: 'Unauthorized',
-      message: 'invalid auth token'
-    });
-  }
-});
 
 app.get('/', (_, res) => res.send('Hello from DIM!!!'));
 
@@ -31,6 +23,17 @@ app.all('*', jwt({ secret: process.env.JWT_SECRET! }));
 app.get('/test', (req, res) =>
   res.send(`Secret squirrel ${JSON.stringify(req.user)}`)
 );
+
+app.use((err, _req, res, next) => {
+  if (err.name === 'UnauthorizedError') {
+    res.status(401).send({
+      status: err.name,
+      message: err.message
+    });
+  } else {
+    next(err);
+  }
+});
 
 metrics.increment('startup', 1);
 app.listen(port, () => console.log(`DIM API started up on port ${port}`));
