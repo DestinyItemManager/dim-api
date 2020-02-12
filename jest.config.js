8ee951c0e7ad570aba7d3de2cd1ad307a2106890
Ben Hollis <ben@benhollis.net>
2020-02-12 00:26:14 -0800

    Few more build fixes

--- jest.config.js
+++ jest.config.js
@@ -2,12 +2,13 @@ const { pathsToModuleNameMapper } = require('ts-jest/utils');
 const { compilerOptions } = require('./tsconfig');
 
 module.exports = {
-  transform: { '\\.(t|j)s$': ['ts-jest'] },
+  transform: { '\\.ts$': ['ts-jest'] },
   preset: 'ts-jest',
   verbose: true,
   moduleNameMapper:
     compilerOptions.paths &&
     pathsToModuleNameMapper(compilerOptions.paths, {
       prefix: '<rootDir>/'
-    })
+    }),
+  modulePathIgnorePatterns: ['<rootDir>/dist/']
 };
