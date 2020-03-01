6af1c3ad5dd2cd5949f513632cb86bc3f75f6134
Ben Hollis <ben@benhollis.net>
2020-02-29 18:54:24 -0800

    Upgrade to a real deploy script

---
+++ kubernetes/deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -ex
+
+ROOT=$(git rev-parse --show-toplevel)
+COMMITHASH=$(git rev-parse HEAD)
+IMAGE="destinyitemmanager/dim-api:$COMMITHASH"
+
+if [[ "$(docker images -q "$IMAGE" 2> /dev/null)" == "" ]]; then
+  rm -rf dist && yarn build:api && docker build -t "$IMAGE" "$ROOT"
+  docker push "$IMAGE"
+fi
+
+mkdir -p "$ROOT/deployment"
+
+cp "$ROOT/kubernetes/dim-api-deployment.yaml" "$ROOT/deployment"
+
+sed -i'' -e "s/\$COMMITHASH/$COMMITHASH/" "$ROOT/deployment/dim-api-deployment.yaml"
+
+kubectl apply -f "$ROOT/deployment/dim-api-deployment.yaml"
+
+rm -rf "$ROOT/deployment"
