ee7a51063594a662187f54a85c2a24b0236192ff
Ben Hollis <ben@benhollis.net>
2020-12-31 18:32:53 -0800

    Add "single character mode"

--- api/shapes/settings.ts
+++ api/shapes/settings.ts
@@ -122,6 +122,9 @@ export interface Settings {
   compareBaseStats: boolean;
   /** Item popup sidecar collapsed just shows icon and no character locations */
   sidecarCollapsed: boolean;
+
+  /** In "Single Character Mode" DIM pretends you only have one (active) character and all the other characters' items are in the vault. */
+  singleCharacter: boolean;
 }
 
 export const defaultSettings: Settings = {
@@ -215,4 +218,6 @@ export const defaultSettings: Settings = {
 
   compareBaseStats: false,
   sidecarCollapsed: false,
+
+  singleCharacter: false,
 };
