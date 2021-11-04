5d4cda75f3fb679c3fc5594519b58c3738190b94
Ben Hollis <ben@benhollis.net>
2021-11-04 13:52:25 -0700

    Revert "Add notes to loadouts"
    This reverts commit 5a01075c3f5b3af7d6c3af30163fe57b87c57833.

--- api/shapes/loadouts.ts
+++ api/shapes/loadouts.ts
@@ -1,36 +1,28 @@
 import { DestinyClass } from 'bungie-api-ts/destiny2';
 
 export interface LoadoutItem {
-  /** itemInstanceId of the item (if it's instanced) */
+  // itemInstanceId of the item (if it's instanced)
   id?: string;
-  /** DestinyInventoryItemDefinition hash of the item */
+  // DestinyInventoryItemDefinition hash of the item
   hash: number;
-  /** Optional amount (for consumables), default to zero */
+  // Optional amount (for consumables), default to zero
   amount?: number;
 }
 
 export interface Loadout {
-  /**
-   * A globally unique (UUID) identifier for the loadout.
-   * Chosen by the client
-   */
+  // A globally unique (UUID) identifier for the loadout.
+  // Chosen by the client
   id: string;
   name: string;
-  /** Optional longform notes about the loadout. */
-  notes?: string;
-  /**
-   * DestinyClass enum value for the class this loadout is restricted
-   * to. This is optional (set to Unknown for loadouts that can be used anywhere).
-   */
+  // DestinyClass enum value for the class this loadout is restricted
+  // to. This is optional (set to Unknown for loadouts that can be used anywhere).
   classType: DestinyClass;
-  /**
-   * DestinyInventoryItemDefinition hash of an emblem to use as
-   * an icon for this loadout
-   */
+  // DestinyInventoryItemDefinition hash of an emblem to use as
+  // an icon for this loadout
   emblemHash?: number;
-  /** Whether to clear out other items when applying this loadout */
+  // Whether to clear out other items when applying this loadout
   clearSpace: boolean;
-  /** Lists of equipped and unequipped items in the loadout */
+  // Lists of equipped and unequipped items in the loadout
   equipped: LoadoutItem[];
   unequipped: LoadoutItem[];
   /** Information about the desired properties of this loadout - used to drive the Loadout Optimizer or apply Mod Loadouts */
