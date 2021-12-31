3d4d9ace22e97e707f0ea70d698c9b471a0e715a
Ben Hollis <ben@benhollis.net>
2021-12-31 15:33:19 -0500

    Updated types for an activity tag experiment

--- api/shapes/item-annotations.ts
+++ api/shapes/item-annotations.ts
@@ -1,4 +1,9 @@
-export type TagValue = 'favorite' | 'keep' | 'infuse' | 'junk' | 'archive';
+export type TagValue = "favorite" | "keep" | "infuse" | "junk" | "archive";
+
+enum TagVariant {
+  PVP = 1,
+  PVE = 2,
+}
 
 /** Any extra info added by the user to individual items - tags, notes, etc. */
 export interface ItemAnnotation {
@@ -8,14 +13,15 @@ export interface ItemAnnotation {
   tag?: TagValue | null;
   /** Optional text notes on the item. */
   notes?: string | null;
+  /**
+   * An optional "variant" for the tag that only has meaning if the tag is set.
+   * This provides a backwards and forwards compatible way to say a roll is
+   * "Keep-PVP" or "Keep-PVE". Clients that don't understand this flag will
+   * simply show "Keep". This is only really meant to be used with the "keep"
+   * tag.
+   */
+  v?: TagVariant;
 }
 
-/** Any extra info added by the user to item hashes (shaders, basically) */
-export interface ItemHashTag {
-  /** The inventory item hash for an item */
-  hash: number;
-  /** Optional tag for the item. */
-  tag?: TagValue | null;
-  /** Optional text notes on the item. */
-  notes?: string | null;
-}
+/** Any extra info added by the user to item hashes (shaders and mods) */
+export type ItemHashTag = Omit<ItemAnnotation, "id">;
