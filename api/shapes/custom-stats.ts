80e159754331ed5878f894b8e8c7534b8354f49c
nev <gh@f-m.fm>
2023-02-21 06:00:10 -0800

    some fixes after reality had its way with the DIM code

--- api/shapes/custom-stats.ts
+++ api/shapes/custom-stats.ts
@@ -1,8 +1,7 @@
-// we go generous here on key options, because sometimes a statHash
-// is a string, because it was a dictionary key
-
 import { DestinyClass } from 'bungie-api-ts/destiny2';
 
+// we go generous here on key options, because sometimes a statHash
+// is a string, because it was a dictionary key
 /** traditional custom stats use a binary 1 or 0 for all 6 armor stats, but this could support more complex weights */
 export interface CustomStatWeights {
   [statHash: number]: number | undefined;
@@ -10,13 +9,13 @@ export interface CustomStatWeights {
 }
 
 export interface CustomStatDef {
-  /** a unique identifier to look up this stat, uuidv4 for new stats, or a special string for up-converted old cust stats */
-  id: string;
+  /** a unique-per-user fake statHash used to look this stat up */
+  statHash: number;
   /** a unique-per-class name for this stat */
   label: string;
   /** an abbreviated/crunched form of the stat label, for use in search filters */
   shortLabel: string;
-  /** which guardian class this stat should be used for */
+  /** which guardian class this stat should be used for. DestinyClass.Unknown makes a global (all 3 classes) stat */
   class: DestinyClass;
   /** info about how to calculate the stat total */
   weights: CustomStatWeights;
