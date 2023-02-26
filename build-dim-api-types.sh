ae0fc84830ed363bc37ccfb444439868df9359ca
Ben Hollis <ben@benhollis.net>
2023-02-26 12:38:38 -0800

    Revert "Publish esm version of dim-api-types"
    This reverts commit 384849d1b7078c781da7ac40acc1550f82d80a5a.

--- build-dim-api-types.sh
+++ build-dim-api-types.sh
@@ -9,7 +9,6 @@ node ./transform-dim-api-types.js > api/shapes/index.ts
 
 babel ./api/shapes/**/* --out-dir dim-api-types --extensions ".ts"
 tsc -p tsconfig.dim-api-types.json
-babel --no-babelrc --config-file ./babel-esm.config.json ./api/shapes/**/* --out-dir dim-api-types/esm --extensions ".ts"
 
 rm -f api/shapes/index.ts
 
