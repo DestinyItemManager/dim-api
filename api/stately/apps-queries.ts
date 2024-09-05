7849d965bb4e9bdd10c073087f18b193ee248116
Ben Hollis <ben@benhollis.net>
2024-09-05 11:48:25 -0700

    Use the apps queries

--- api/stately/apps-queries.ts
+++ api/stately/apps-queries.ts
@@ -71,13 +71,38 @@ export async function updateApps(token: ListToken, apps: ApiApp[]): Promise<[Api
  * Get an app by its ID.
  */
 export async function getAppById(id: string): Promise<ApiApp | undefined> {
-  return client.get('ApiApp', keyPath`/apps-1/app-${id}`);
+  return client.get('ApiApp', keyPathFor(id));
 }
 
 /**
  * Insert a new app into the list of registered apps, or update an existing app.
  */
 export async function insertApp(app: ApiApp): Promise<ApiApp> {
-  // TODO: wish I could set an if-not-exists condition here, to avoid accidentally updating an app
-  return client.put(client.create('ApiApp', app));
+  let resultApp: ApiApp | undefined;
+  // TODO: wish I could set an if-not-exists condition here, to avoid
+  // accidentally updating an app. Instead I got a transaction.
+  const result = await client.transaction(async (txn) => {
+    resultApp = await txn.get('ApiApp', keyPathFor(app.id));
+    if (app) {
+      return;
+    }
+    txn.put(client.create('ApiApp', app));
+  });
+
+  if (resultApp) {
+    return resultApp;
+  }
+
+  if (result.committed && result.puts.length === 1) {
+    const put = result.puts[0];
+    if (client.isType(put, 'ApiApp')) {
+      return put;
+    }
+  }
+
+  throw new Error('No ApiApp in result!');
+}
+
+function keyPathFor(id: string) {
+  return keyPath`/apps-1/app-${id}`;
 }
