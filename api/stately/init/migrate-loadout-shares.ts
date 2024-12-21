210ef5c92f72be6308af1491c1f25be86aac9763
Ben Hollis <ben@benhollis.net>
2024-12-21 11:20:50 -0800

    Remove loadout shares PG impl

--- api/stately/init/migrate-loadout-shares.ts
+++ api/stately/init/migrate-loadout-shares.ts
@@ -1,6 +1,6 @@
-import { migrateLoadoutShareChunk } from '../migrator/loadout-shares.js';
+// import { migrateLoadoutShareChunk } from '../migrator/loadout-shares.js';
 
-while (true) {
-  await migrateLoadoutShareChunk();
-  console.log('Migrated loadout shares');
-}
+// while (true) {
+//   await migrateLoadoutShareChunk();
+//   console.log('Migrated loadout shares');
+// }
