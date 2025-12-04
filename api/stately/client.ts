25f2b54c95caa429898a4f89cfe90d6d382dd2d8
Ben Hollis <ben@benhollis.net>
2025-12-04 14:21:18 -0800

    Upgrade client signature

--- api/stately/client.ts
+++ api/stately/client.ts
@@ -1,8 +1,11 @@
+import { nodeTransport } from '@stately-cloud/client/node';
 import { createClient } from './generated/index.js';
 
 /**
  * Our StatelyDB client, bound to our types and store.
  */
-export const client = createClient(BigInt(process.env.STATELY_STORE_ID!), {
+export const client = createClient({
+  storeId: BigInt(process.env.STATELY_STORE_ID!),
   region: process.env.STATELY_REGION || 'us-west-2',
+  transport: nodeTransport,
 });
